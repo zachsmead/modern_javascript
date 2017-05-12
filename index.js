2dde71d89a108e4bfce62fe51ec4050cbe371c82
= <zrsmead@gmail.com>
2017-05-11 19:45:51 -0700

    added npm server

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ var moment = require('moment');
 console.log("Hello from Javascript!");
 var time = moment().format('MMMM Do YYYY, h:mm:ss a');
 console.log(time);
+
+console.log("Live update successful");
